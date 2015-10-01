c0fe85c033353f98c5eb6d5ce51deb4089ae6449
Shunichi Yamamoto <info@tkrworks.net>
2015-10-01 16:20:49 +0900

    First Commit

---
+++ cosc.h
@@ -0,0 +1,28 @@
+/*
+ * Copylight (C) 2015, Shunichi Yamamoto, tkrworks.net
+ *
+ * This file is part of COSC.
+ *
+ * COSC is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option ) any later version.
+ *
+ * COSC is distributed in the hope that it will be useful,
+ * but WITHIOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.   See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PICrouter. if not, see <http:/www.gnu.org/licenses/>.
+ *
+ * cosc.h,v.0.1.0 2015/10/01
+ */
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netinet/in.h>
+
+#define MAX_MESSAGE_LEN 256// 160
